7419bebce0ccf0e5a2ff4ef2814182afbb27e5ae
ElsayedAhmed <hasoubaty@gmail.com>
2016-05-29 16:41:03 +0200

    hiiden files to tring to solve error

---
+++ database/migrations/2016_05_29_141237_create_medicines_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedicinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicines', function (Blueprint $table) {
+            $table->increments('id');
+            // $table->string('name',100);
+            // $table->enum('types', array('foo', 'bar'));
+            // $table->string('company');
+
+            // $table->increments('id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('medicines');
+    }
+}
